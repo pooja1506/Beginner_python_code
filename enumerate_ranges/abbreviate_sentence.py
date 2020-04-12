9a3126bfa09d828bd026f97c30229c4d0a8ccc59
Pooja Mehta <pooja.dmehta15@gmail.com>
2020-04-12 18:22:44 +0530

    to remove vowels

---
+++ enumerate_ranges/abbreviate_sentence.py
@@ -0,0 +1,27 @@
+""" Write a method abbreviate_sentence that takes in a sentence string and returns a new sentence where every word longer than 4 characters has all of it's vowels removed. """
+
+
+def abbreviate_sentence(sent):
+    words = sent.split(" ")
+    new_words = []
+    for each_word in words:
+        if len(each_word) > 4:
+            word_without_vowels = sentence(each_word)
+            new_words.append(word_without_vowels)
+        else:
+            new_words.append(each_word)
+        new_sentence = " ".join(new_words)
+    return new_sentence
+
+
+def sentence(each_word):
+    vowels = 'aeiou'
+    no_vowels = ''
+    for each_char in each_word:
+        if not (each_char in vowels):
+            no_vowels += each_char
+    return no_vowels
+
+
+print(abbreviate_sentence("follow the yellow brick road")) # => "fllw the yllw brck road"
+print(abbreviate_sentence("what a wonderful life"))  # => "what a wndrfl life"
--- enumerate_ranges/to_initials.py
+++ enumerate_ranges/to_initials.py
@@ -1,18 +1,20 @@
 """ Write a method to_initials that takes in a person's name string and returns a string representing their initials."""
+
+
 def to_initials(name):
-    value = ""
-    for index,items in enumerate(name):
-        for character in items:
-            if character == character.upper():
-        
-                value = value + character
+    value = []
+    new_name = ""
+    split_name = name.split(" ")
+    i = 0
 
-    return value
+    while i < len(split_name):
+        x = (split_name[i])[0]
+        value.append(x)
+        i = i+1
+    new_name = "".join(map(str, value))
+    return new_name
 
-
-
-        
-
-print(to_initials("Kelvin Bridges"))      # => "KB" 
-print(to_initials("Michaela Yamamoto"))   # => "MY" 
+print(to_initials("Kelvin Bridges"))      # => "KB"
+print(to_initials("Michaela Yamamoto"))   # => "MY"
 print(to_initials("Mary La Grange"))      # => "MLG"
+print(to_initials("pooja mehta"))
