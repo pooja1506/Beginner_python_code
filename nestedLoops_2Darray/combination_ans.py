c4b3d5d79cf81e410f50da7783d66e3aa0828bd4
Pooja Mehta <pooja.dmehta15@gmail.com>
2020-04-24 12:43:46 +0530

    combinations of array in two dimensional

--- nestedLoops_2Darray/combination_ans.py
+++ nestedLoops_2Darray/combination_ans.py
@@ -1,6 +1,17 @@
 """ Write a method combinations that takes in an array of unique elements, the method should return a 2D array representing all possible combinations of 2 elements of the array. """
-def combinations_ans(arr): 
-	pass 
+from itertools import combinations
+def combinations_ans(arr):
+    list2 = []
+    i=0
+    x = list(combinations(arr,2))
+
+    while i < len(x):
+        list2.append(list(x[i]))
+        i=i+1
+
+    return list2
+
+        
 
 # => [ [ "a", "b" ], [ "a", "c" ], [ "b", "c" ] ] 
 print(combinations_ans(["a", "b", "c"])) 
