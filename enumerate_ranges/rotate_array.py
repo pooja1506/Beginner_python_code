fa36fa401e574e38006b12b30147a02c220a249d
Pooja Mehta <pooja.dmehta15@gmail.com>
2020-04-24 11:55:39 +0530

    rotation of array

--- enumerate_ranges/rotate_array.py
+++ enumerate_ranges/rotate_array.py
@@ -1,8 +1,25 @@
 '''Write a method rotate_array that takes in an array and a number. The method should return the array after rotating the elements the specified number of times. A single rotation takes the last element of the array and moves it to the front.'''
-def rotate_array(arr, num):
-    for i in range(len(arr)):
-        new = arr[i]
-        
+from collections import deque
 
-print(rotate_array([ "Matt", "Danny", "Mashu", "Matthias" ], 1)) # => [ "Matthias", "Matt", "Danny", "Mashu" ]
-#print(rotate_array([ "a", "b", "c", "d" ], 2)) # => [ "c", "d", "a", "b" ]
+
+def rotate_array(arr, num):
+    i = 0
+    while i < num:
+        ele = arr.pop()
+        print("pop: ", ele)
+        arr = deque(arr)
+        print("arr_deque: ", arr)
+        arr.appendleft(ele)
+        print("arr_append_left: ", arr)
+        arr = list(arr)
+        print("arr_list: ", arr)
+        print("-------------------**************---------------------")
+        i += 1
+    return arr
+
+# => [ "Matthias", "Matt", "Danny", "Mashu" ]
+print(rotate_array(["Matt", "Danny", "Mashu", "Matthias"], 1))
+
+print(rotate_array(["a", "b", "c", "d"], 2))  # => [ "c", "d", "a", "b" ]
+
+print(rotate_array(["10", "20", "30", "40", "50", "60"], 4))
---
+++ nestedLoops_2Darray/combination_ans.py
@@ -0,0 +1,9 @@
+""" Write a method combinations that takes in an array of unique elements, the method should return a 2D array representing all possible combinations of 2 elements of the array. """
+def combinations_ans(arr): 
+	pass 
+
+# => [ [ "a", "b" ], [ "a", "c" ], [ "b", "c" ] ] 
+print(combinations_ans(["a", "b", "c"])) 
+
+# => [ [ 0, 1 ], [ 0, 2 ], [ 0, 3 ], [ 1, 2 ], [ 1, 3 ], [ 2, 3 ] ] 
+print(combinations_ans([0, 1, 2, 3]))
