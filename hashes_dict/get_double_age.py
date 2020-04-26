599eb688c72388247e3c838cfa102e4b575f3554
Pooja Mehta <pooja.dmehta15@gmail.com>
2020-04-26 13:52:53 +0530

    doubling of age in dictionary

--- hashes_dict/get_double_age.py
+++ hashes_dict/get_double_age.py
@@ -1,6 +1,8 @@
 """ Write a method get_double_age that takes in a dictionary and returns twice the "age" value of the dictionary. """ 
 def get_double_age(hash_in):
-    
+    a = hash_in["age"]
+    age_double = a * 2
+    return age_double
 
 print(get_double_age({"name": "App Academy", "age": 5}))  # => 10 
 print(get_double_age({"name": "Ruby", "age": 23}))       # => 46
