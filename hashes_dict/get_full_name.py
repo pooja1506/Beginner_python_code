06f086d9cece3982d4faa7c097284eb369e6c491
Pooja Mehta <pooja.dmehta15@gmail.com>
2020-04-26 13:59:59 +0530

    to get the full name with title

---
+++ hashes_dict/get_full_name.py
@@ -0,0 +1,11 @@
+""" Write a method get_full_name that takes in a dictionary containing a first, last, and title. The method should return a string representing the dictionary's full name """
+def get_full_name(hash_in):
+    name_access = hash_in["first"] + " " + hash_in["last"] + ", the " + hash_in["title"]
+
+    return name_access
+
+hash1 = {"first": "Michael", "last": "Jordan", "title": "GOAT"} 
+print(get_full_name(hash1))  # => "Michael Jordan, the GOAT" 
+
+hash2 = {"first": "Fido", "last": "McDog", "title": "Loyal"} 
+print(get_full_name(hash2))  # => "Fido McDog, the Loyal"
