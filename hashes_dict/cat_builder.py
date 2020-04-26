ca7f7ace81e3b2a81f26be21c61038335a917bff
Pooja Mehta <pooja.dmehta15@gmail.com>
2020-04-26 18:22:53 +0530

    name , color and age in dictionary

--- hashes_dict/cat_builder.py
+++ hashes_dict/cat_builder.py
@@ -1,5 +1,11 @@
 """ Write a method cat_builder that takes in a name, color, and age. The method should return a dictionary representing a cat with those values. """
 def cat_builder(name_str, color_str, age_num):
+    final_dict = {}
+    final_dict["name"] = name_str
+    final_dict["color"] = color_str
+    final_dict["age"] = age_num
+
+    return final_dict
 	
 
 # => {"name": "Whiskers", "color": "orange", "age": 3} 
