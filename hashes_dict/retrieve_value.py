e144d9e56e6ec26049c47c772cc4a27e36053d87
Pooja Mehta <pooja.dmehta15@gmail.com>
2020-04-26 16:50:56 +0530

    to take in key and return corresponding values

--- hashes_dict/retrieve_value.py
+++ hashes_dict/retrieve_value.py
@@ -1,9 +1,12 @@
 """ Write a method retrieve_values that takes in two dictionaries and a key. The method should return an array containing the values from the two hashes that correspond with the given key. """
 def retrieve_values(hash1, hash2, key):
     list1 = []
-    list2 = []
-    a = hash["name"]
-    list1.append(a)
+    if key in hash1 and hash2:
+        list1.append(hash1[key])
+        list1.append(hash2[key])
+
+    return list1
+
 
 dog1 = {"name": "Fido", "color": "brown"} 
 dog2 = {"name": "Spot", "color": "white"} 
